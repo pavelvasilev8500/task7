cbe414db7af0dd22b2a0a1899b6b8672192ece48
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2020-09-06 21:25:33 +0300

    Create Customers.cs

---
+++ ConsoleApp1/Customers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Order { get; set; }
+        public int Money { get; set; }
+    }
+}
