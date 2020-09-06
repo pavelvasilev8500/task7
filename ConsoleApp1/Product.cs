721cf87c0b9a82b43f0db9fd1111ea84138704c3
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2020-09-06 21:26:56 +0300

    Create Product.cs

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Product
+    {
+        public string it;
+        public string it1;
+        public int co;
+        public int co1;
+
+        public void Water()
+        {
+            var item = "Water";
+            var cost = 11;
+            var product = new Products
+            {
+                Item = item,
+                Cost = cost
+            };
+            it = item;
+            co = cost;
+        }
+
+        public void Milk()
+        {
+            var item1 = "Milk";
+            var cost1 = 14;
+            var product1 = new Products
+            {
+                Item = item1,
+                Cost = cost1
+            };
+            it1 = item1;
+            co1 = cost1;
+        }
+    }
+}
