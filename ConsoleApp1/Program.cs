529c7b615d05e3f7787c0cdb40e4c350707adfc5
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2020-09-06 21:27:04 +0300

    Create Program.cs

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+            shop.shop();
+        }
+    }
+}
